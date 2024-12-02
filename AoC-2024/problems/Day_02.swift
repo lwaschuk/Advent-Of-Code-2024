c79c0b46f49ac8eb65789b5e1f87a803afac148b
Lukas Waschuk <77379787+lwaschuk@users.noreply.github.com>
2024-12-01 22:42:02 -0700

    Day 2

--- AoC-2024/problems/Day_02.swift
+++ AoC-2024/problems/Day_02.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Lukas Waschuk on 11/25/24.
 //
+import Foundation
 
 class Day_02 {
   //  let input: [String] = File_Utils().readFile(named: "d2_e1", withExtension: "txt")
@@ -17,14 +18,60 @@ class Day_02 {
   }
 
   func part1() {
+    var safeCount = 0
 
+    for line in helper.deserialize(input) {
+      if strictlyDecreasing(line) || strictlyIncreasing(line) { safeCount += 1 }
+    }
+
+    print("Part 1: \(safeCount)")
   }
 
   func part2() {
+    var safeCount = 0
 
+    for line in helper.deserialize(input) {
+      var foundSafeVariation: Bool = false
+
+      for line in getVariations(line) {
+        if strictlyDecreasing(line) || strictlyIncreasing(line) { foundSafeVariation = true }
+      }
+
+      if foundSafeVariation { safeCount += 1 }
+    }
+
+    print("Part 2: \(safeCount)")
+  }
+
+  func getVariations(_ input: [Int]) -> [[Int]] {
+    var variations: [[Int]] = []
+    for i in 0..<input.count {
+      var variation = input
+      variation.remove(at: i)
+      variations.append(variation)
+    }
+    return variations
+  }
+
+  func strictlyIncreasing(_ array: [Int]) -> Bool {
+    let diffArray = zip(array.dropFirst(), array).map({ $0.0 - $0.1 })
+    return diffArray.filter({ $0 >= -3 && $0 < 0 }).count == array.count - 1
+  }
+
+  func strictlyDecreasing(_ array: [Int]) -> Bool {
+    let diffArray = zip(array.dropFirst(), array).map({ $0.0 - $0.1 })
+    return diffArray.filter({ $0 <= 3 && $0 > 0 }).count == array.count - 1
   }
 
   internal class Helper {
-
+    func deserialize(_ input: [String]) -> [[Int]] {
+      var array: [[Int]] = []
+      for line in input {
+        guard !line.isEmpty else { continue }
+        let split = line.split(separator: " ")
+        array.append(split.map({ Int($0)! }))
+      }
+      return array
+    }
   }
 }
