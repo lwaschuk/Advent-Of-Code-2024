c1a6c7e39beb73ee10814f41824ed070a117361a
Lukas Waschuk <77379787+lwaschuk@users.noreply.github.com>
2024-12-09 22:52:19 -0700

    Day 10

--- AoC-2024/problems/Day_10.swift
+++ AoC-2024/problems/Day_10.swift
@@ -4,27 +4,117 @@
 //
 //  Created by Lukas Waschuk on 11/25/24.
 //
+import os
+import Foundation
 
 class Day_10 {
   //  let input: [String] = File_Utils().readFile(named: "d10_e1", withExtension: "txt")
   //  let input: [String] = File_Utils().readFile(named: "d10_e2", withExtension: "txt")
   let input: [String] = File_Utils().readFile(named: "d10", withExtension: "txt")
   let helper = Helper()
-
+  var rows: Int = 0
+  var columns: Int = 0
+  
   func run() {
     part1()
     part2()
   }
-
-  func part1() {
-
+  
+  func part1(runP2: Bool = false) {
+    var map = helper.deserialize(input)
+    rows = map.count
+    columns = map[0].count
+    
+    var ret: [Int] = []
+    
+    var start = setPosition(toFirstOccuranceOf: 0, in: map)
+    
+    while let position = start {
+      var baseMap = map
+      
+      var recursiveCount: Int = 0
+      dfs(map: &baseMap, position: position, recursiveCount: &recursiveCount, part2: runP2)
+      
+      ret.append(recursiveCount)
+      map[position.row][position.column] = -1
+      start = setPosition(toFirstOccuranceOf: 0, in: map)
+    }
+    
+    print("Part \(runP2 ? "2" : "1"): \(ret.reduce(0, +))")
   }
-
+  
   func part2() {
-
+    part1(runP2: true)
   }
-
+  
+  func dfs(map: inout [[Int]], position: Position, recursiveCount: inout Int, part2: Bool = false) {
+    let currentIncline = map[position.row][position.column]
+    guard currentIncline != 9 else {
+      recursiveCount += 1
+      
+      // part 2 is just not invalidating the 9 lmao
+      if !part2 {
+        map[position.row][position.column] = -1
+      }
+      return
+    }
+    
+    let east = position + .EAST
+    if inBounds(position: east) {
+      if map[east.row][east.column] == currentIncline + 1 {
+        dfs(map: &map, position: east, recursiveCount: &recursiveCount, part2: part2)
+      }
+    }
+    
+    let south = position + .SOUTH
+    if inBounds(position: south) {
+      if map[south.row][south.column] == currentIncline + 1 {
+        dfs(map: &map, position: south, recursiveCount: &recursiveCount, part2: part2)
+      }
+    }
+    
+    let west = position + .WEST
+    if inBounds(position: west) {
+      if map[west.row][west.column] == currentIncline + 1 {
+        dfs(map: &map, position: west, recursiveCount: &recursiveCount, part2: part2)
+      }
+    }
+    
+    let north = position + .NORTH
+    if inBounds(position: north) {
+      if map[north.row][north.column] == currentIncline + 1 {
+        dfs(map: &map, position: north, recursiveCount: &recursiveCount, part2: part2)
+      }
+    }
+  }
+  
+  func inBounds(position: Position) -> Bool {
+    position.row >= 0 && position.row < rows && position.column >= 0 && position.column < columns
+  }
+  
+  func setPosition(toFirstOccuranceOf number: Int, in input: [[Int]]) -> Position? {
+    for (index_Y, row) in input.enumerated() {
+      for (index_X, _) in row.enumerated() {
+        if input[index_Y][index_X] == number {
+          return Position(row: index_Y, column: index_X)
+        }
+      }
+    }
+    return nil
+  }
+  
   internal class Helper {
-
+    func deserialize(_ input: [String]) -> [[Int]] {
+      var array: [[Int]] = []
+      for line in input {
+        guard !line.isEmpty else { continue }
+        var temp: [Int] = []
+        for number in line {
+          temp.append(Int(String(number))!)
+        }
+        array.append(temp)
+      }
+      return array
+    }
   }
 }
